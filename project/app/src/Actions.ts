df5ce649e04c85122d62dd941d730dcc75bba37b
Michael Medin <michael.medin@pricer.com>
2020-02-20 07:18:19 +0100

    Removed unused translations

--- project/app/src/Actions.ts
+++ project/app/src/Actions.ts
@@ -2,12 +2,12 @@ export interface IActionHandler {
   /**
    * Set the maximum or minimum temperature bounds
    * @param args.type the type of bound to set #dropdown:{"min":"Minimum", "max":"Maximum"}
-   * @param args.temperature temperature
+   * @param args.temperature $common.temperature
    */
   SetTemperatureBounds(args: { type: string; temperature: number }): boolean;
   /**
    * Set the mode for a given zone
-   * @param args.zone zone #sample:$common.kitchen
+   * @param args.zone $common.zone #sample:$common.kitchen
    * @param args.mode mode #dropdown:{"disabled":"Disabled", "enabled":"Enabled", "monitored":"Monitored"}
    */
   SetZoneMode(args: { zone: string; mode: string }): boolean;
